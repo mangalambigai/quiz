d4d6b0a70e889e8c07472b5a4b902259abf8a388
mangalambigai <mangalambigais@gmail.com>
2016-04-20 14:46:07 -0400

    Shuffle the options, and use col-4 for large screens

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+//util function to shuffle array
+function shuffle(arr) {
+    var len = arr.length;
+    var randomArr = [];
+    for (var i = 0; i < len; i++) {
+        var index = Math.floor(Math.random() * arr.length);
+        randomArr.push(arr[index]);
+        arr.splice(index, 1);
+    }
+    return randomArr;
+}
+
 /**
  * @ngdoc object
  * @name quizApp
@@ -11,19 +23,28 @@ angular.module('quizApp', [])
  * @description
  * Gets randomly generated cheers
  */
-.service('cheer', function() {
-	return ({
-		get: function() {
-		var cheers= [
-				'Awesome!!!',
-				'Fantastic!',
-				'Excellent!!',
-				'Super!',
-				'Way to go!',
-				'You are a genius!'];
-			return cheers[Math.floor(Math.random()*cheers.length)];
-		}
-	});
+.service('cheer', function () {
+    return ({
+        get: function () {
+            var cheers = [
+                'Awesome!!!',
+                'Fantastic!',
+                'Excellent!!',
+                'Super!',
+                'Way to go!',
+                'You are a genius!'
+            ];
+            var symbols = ['✓', '❤', '🎈', '🌠', '👍', '🖒',
+                '🌟', '😊', '🚀', '❀', '🌷', '🌹'
+            ]
+            return {
+                message: cheers[Math.floor(Math.random() *
+                    cheers.length)],
+                symbol: symbols[Math.floor(Math.random() *
+                    symbols.length)]
+            };
+        }
+    });
 })
 
 /**
@@ -33,20 +54,39 @@ angular.module('quizApp', [])
  * @description
  * Gets randomly generated questions
  */
-.service('questions', function() {
-	return ({
-		get: function() {
-			//TODO: get alphabets,
-			//TODO: put in logic to select questions based on previous answers
-			var number = Math.floor(Math.random()*10);
-			return {
-				question: 'Choose the number '+ number,
-				answer: number,
-				//TODO: generate better options
-				options: [number, number+1, number+2]
-			}
-		}
-	})
+.service('questions', function () {
+    return ({
+        get: function () {
+            //TODO: get alphabets,
+            //TODO: put in logic to select questions based on previous answers
+            var number = Math.floor(Math.random() * 10);
+            var optioncount = 3;
+            var options = [];
+
+            options[0] = number;
+            for (var i = 1; i < optioncount; i++) {
+                var findAnotherOption = true;
+                while (findAnotherOption) {
+                    var opt = Math.floor(Math.random() * 10);
+                    //if this option is already choosen, choose another one
+                    findAnotherOption = false;
+                    for (var j = 0; j < i; j++) {
+                        if (opt === options[j])
+                            findAnotherOption = true;
+                    }
+                    if (!findAnotherOption)
+                        options[i] = opt;
+                }
+            }
+            options = shuffle(options);
+
+            return {
+                question: 'Choose the number ' + number,
+                answer: number,
+                options: options
+            }
+        }
+    })
 })
 
 /**
@@ -56,19 +96,17 @@ angular.module('quizApp', [])
  * @description
  * Controller for the quiz page
  */
-.controller('QuizCtrl', ['cheer', 'questions', function(cheer, questions) {
+.controller('QuizCtrl', ['cheer', 'questions', function (cheer, questions) {
 
-	this.clicked = function(option) {
-		if (option == this.question.answer)
-		{
-			//todo: flash instead of alert
-			this.message = cheer.get();
-			this.question = questions.get();
-		}
-		else
-		{
-			//TODO: Flash the correct answer
-		}
-	}
-	this.question = questions.get();
+    this.clicked = function (option) {
+        if (option == this.question.answer) {
+            var cheerMsg = cheer.get();
+            this.message = cheerMsg.message;
+            this.symbol = cheerMsg.symbol;
+            this.question = questions.get();
+        } else {
+            //TODO: Flash the correct answer
+        }
+    }
+    this.question = questions.get();
 }]);
