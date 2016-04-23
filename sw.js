2315a9d356b05fcc5586a402f8d866df37fc579f
mangalambigai <mangalambigais@gmail.com>
2016-04-23 10:55:44 -0400

    Cache all responses

--- sw.js
+++ sw.js
@@ -7,20 +7,20 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCacheName)
         .then(function(cache) {
-            console.log('Adding cache');
-            //non critical files
-            cache.addAll([
+            //critical resources!!!
+            return cache.addAll([
+                'index.html',
+                'js/all.js',
+                'js/lib/angular.js',
+                'js/lib/angular-route.js',
+                'style/style.css',
+                'data/shapes.json',
+                'partials/main.html',
+                'partials/quizthree.html',
                 'data/smallletters1.json',
                 'data/counting.json',
                 'data/numbers.json',
                 'data/bigletters1.json'
-                ]);
-            //critical
-            return cache.addAll([
-                'index.html',
-                'js/all.js',
-                'style/style.css',
-                'data/shapes.json'
             ]);
         })
     );
@@ -35,7 +35,7 @@ self.addEventListener('activate', function(event) {
             return Promise.all(
                 cacheNames.filter(function(cacheName) {
                     return cacheName.startsWith('psquiz-') &&
-                        cacheName !== staticCacheName;
+                        cacheName != staticCacheName;
                 }).map(function(cacheName) {
                     return caches.delete(cacheName);
                 })
@@ -48,6 +48,22 @@ self.addEventListener('activate', function(event) {
  * Responds with data from cache when the file/data is available
  */
 self.addEventListener('fetch', function(event) {
+
+    //Cache all the responses, except for browser-sync messages
+    event.respondWith(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function(response) {
+                    if (event.request.url.indexOf('browser-sync')== -1)
+                        cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+
+/*
+//This will cache only same location origins
     var requestUrl = new URL(event.request.url);
 
     if (requestUrl.origin === location.origin) {
@@ -58,6 +74,8 @@ self.addEventListener('fetch', function(event) {
             })
         );
     }
+
+    */
 });
 
 /**
