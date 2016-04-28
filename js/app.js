6fd799fd586855118860f7b57df268ba2251b510
mangalambigai <mangalambigais@gmail.com>
2016-04-28 08:31:16 -0400

    Move service worker initialization to a factory, and add code to subscribe to push

--- js/app.js
+++ js/app.js
@@ -33,10 +33,14 @@ angular.module('quizApp', ['ngRoute'])
         controller: 'MainCtrl',
         controllerAs: 'menu'
     })
+    .when('/parents', {
+        templateUrl: 'partials/parents.html',
+        controller: 'ParentCtrl',
+        controllerAs: 'settings'
+    })
     .otherwise({
         redirectTo: '/home'
     });
-
 }])
 /**
  * @ngdoc service
@@ -212,11 +216,12 @@ angular.module('quizApp', ['ngRoute'])
             attempted: 0
         };
 
-        this.init = function() {
-            questions.getList('data/' + $routeParams.id).then(function (list) {
-                vm.questionList = list.data;
-                vm.askNextQuestion();
-            });
+        this.init = function () {
+            questions.getList('data/' + $routeParams.id).then(
+                function (list) {
+                    vm.questionList = list.data;
+                    vm.askNextQuestion();
+                });
         };
 
 
@@ -272,30 +277,30 @@ angular.module('quizApp', ['ngRoute'])
        rocket
        paw
        */
-
 }])
 /**
  * @ngdoc controller
  * @name ServiceController
  *
  * @description
- * Controller for service worker and its updates
+ * Controller for service worker updates
  */
-.controller('ServiceController', ['$scope', function ($scope) {
-
+.controller('ServiceController', ['$scope', 'swfactory',
+    function ($scope, swfactory) {
     /**
      * Starts the service worker
      */
     $scope.init = function () {
         $scope.newversion = false;
+        swfactory.startsw();
+        /*
         if (!navigator.serviceWorker) return;
 
         var swpath = window.location.pathname + 'sw.js';
 
         console.log('pathname: ' + swpath);
 
-        navigator.serviceWorker.register(swpath).then(function (
-            reg) {
+        navigator.serviceWorker.register(swpath).then( function (reg) {
             if (!navigator.serviceWorker.controller) {
                 return;
             }
@@ -325,6 +330,7 @@ angular.module('quizApp', ['ngRoute'])
                 window.location.reload();
                 refreshing = true;
             });
+          */
     };
 
     /**
@@ -358,4 +364,244 @@ angular.module('quizApp', ['ngRoute'])
         });
     };
 
+}])
+/**
+ * @ngdoc factory
+ * @name swfactory
+ *
+ * @description
+ * Handles the service worker instantiation and subscription.
+ */
+.factory('swfactory', function () {
+    var swfactory = {
+        enablePush: false,
+        isSubscribed: false
+    };
+
+    /*
+     * start the serviceworker
+     */
+    swfactory.startsw = function () {
+        if ('serviceWorker' in navigator) {
+            console.log('Service Worker is supported');
+            navigator.serviceWorker.register('sw.js').then(function () {
+                return navigator.serviceWorker.ready;
+            }).then(function (serviceWorkerRegistration) {
+                reg = serviceWorkerRegistration;
+                swfactory.enablePush = true;
+                console.log('Service Worker is ready :^)',
+                    reg);
+            }).catch(function (error) {
+                console.log('Service Worker Error :^(',
+                    error);
+            });
+            //TODO: use initialiseState to set current subscription status
+        }
+    };
+
+    // Once the service worker is registered set the initial state
+    swfactory.initialiseState = function () {
+        // Are Notifications supported in the service worker?
+        if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+            console.warn('Notifications aren\'t supported.');
+            return;
+        }
+
+        // Check the current Notification permission.
+        // If its denied, it's a permanent block until the
+        // user changes the permission
+        if (Notification.permission === 'denied') {
+            console.warn('The user has blocked notifications.');
+            return;
+        }
+
+        // Check if push messaging is supported
+        if (!('PushManager' in window)) {
+            console.warn('Push messaging isn\'t supported.');
+            return;
+        }
+
+        // We need the service worker registration to check for a subscription
+        navigator.serviceWorker.ready.then(function (
+            serviceWorkerRegistration) {
+            // Do we already have a push message subscription?
+            serviceWorkerRegistration.pushManager.getSubscription()
+                .then(function (subscription) {
+                    // Enable any UI which subscribes / unsubscribes from
+                    // push messages.
+                    this.enableSubscribe = false;
+
+                    if (!subscription) {
+                        // We aren't subscribed to push, so set UI
+                        // to allow the user to enable push
+                        return;
+                    }
+
+                    // Keep your server in sync with the latest subscriptionId
+                    sendSubscriptionToServer(
+                        subscription);
+
+                    // Set your UI to show they have subscribed for
+                    // push messages
+                    this.buttonText =
+                        'Disable Push Messages';
+                    isPushEnabled = true;
+                })
+                .catch(function (err) {
+                    console.warn(
+                        'Error during getSubscription()',
+                        err);
+                });
+        });
+    }
+
+    //returns if push is allowed
+    swfactory.getEnablePush = function () {
+        return this.enablePush;
+    };
+
+    //subscribes to push notification
+    swfactory.subscribe = function () {
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).
+        then(function (pushSubscription) {
+            sub = pushSubscription;
+            console.log('Subscribed! Endpoint:', sub.endpoint);
+            swfactory.isSubscribed = true;
+            //TODO: save endpoint in a server
+        });
+    };
+
+    //unsubscribes from push
+    swfactory.unsubscribe = function () {
+        sub.unsubscribe().then(function (event) {
+            console.log('Unsubscribed!', event);
+            swfactory.isSubscribed = false;
+            //TODO: remove endpoint from server
+        }).catch(function (error) {
+            console.log('Error unsubscribing', error);
+            //subscribeButton.textContent = 'Subscribe';
+        });
+    };
+
+    //returns current subscription status
+    swfactory.getSubscription = function () {
+        return swfactory.isSubscribed;
+    }
+
+    return swfactory;
+})
+/**
+ * @ngdoc controller
+ * @name ParentCtrl
+ *
+ * @description
+ * Controller for settings page to be used by parents
+ */
+.controller('ParentCtrl', ['$scope', 'swfactory', function ($scope,
+    swfactory) {
+
+    this.isSubscribed = false;
+    this.save = function () {
+
+    };
+
+    this.init = function () {
+        //TODO: set initial state of push notifications
+        this.enableSubscribe = true; //swservice.getEnablePush();
+        this.buttonText = "Subscribe";
+    };
+
+
+    this.subscribe = function () {
+        //TODO: handle current state of subscription
+        this.enableSubscribe = false;
+        swfactory.subscribe();
+        this.enableSubscribe = true;
+        /*
+        // Disable the button so it can't be changed while
+        //   we process the permission request
+        this.enableSubscribe = true;
+
+        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true})
+            .then(function(subscription) {
+                // The subscription was successful
+                isPushEnabled = true;
+                this.buttonText = 'Disable Push Messages';
+                this.enableSubscribe = false;
+
+                // TODO: Send the subscription subscription.endpoint
+                // to your server and save it to send a push message
+                // at a later date
+                return sendSubscriptionToServer(subscription);
+            })
+            .catch(function(e) {
+                if (Notification.permission === 'denied') {
+                  // The user denied the notification permission which
+                  // means we failed to subscribe and the user will need
+                  // to manually change the notification permission to
+                  // subscribe to push messages
+                  window.Demo.debug.log('Permission for Notifications was denied');
+                  this.enableSubscribe = true;
+                } else {
+                  // A problem occurred with the subscription, this can
+                  // often be down to an issue or lack of the gcm_sender_id
+                  // and / or gcm_user_visible_only
+                  window.Demo.debug.log('Unable to subscribe to push.', e);
+                  this.enableSubscribe = false;
+                  this.buttonText = 'Enable Push Messages';
+                }
+            });
+          });
+        */
+    }
+
+    //TODO: this will probably go into the swfactory
+    this.unsubscribe = function () {
+        swfactory.unsubscribe();
+        /*  this.enableSubscribe = true;
+
+          navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+            // To unsubscribe from push messaging, you need get the
+            // subscription object, which you can call unsubscribe() on.
+            serviceWorkerRegistration.pushManager.getSubscription()
+              .then(function(pushSubscription) {
+                // Check we have a subscription to unsubscribe
+                if (!pushSubscription) {
+                  // No subscription object, so set the state
+                  // to allow the user to subscribe to push
+                  isPushEnabled = false;
+                  this.enableSubscribe = false;
+                  this.buttonText = 'Enable Push Messages';
+                  return;
+                }
+
+                // TODO: Make a request to your server to remove
+                // the users data from your data store so you
+                // don't attempt to send them push messages anymore
+
+                // We have a subscription, so call unsubscribe on it
+                pushSubscription.unsubscribe()
+                  .then(function(successful) {
+                    this.enableSubscribe = false;
+                    this.buttonText = 'Enable Push Messages';
+                    isPushEnabled = false;
+                  }).catch(function(e) {
+                    // We failed to unsubscribe, this can lead to
+                    // an unusual state, so may be best to remove
+                    // the users data from your data store and
+                    // inform the user that you have done so
+
+                    console.log('Unsubscription error: ', e);
+                    this.enableSubscribe = false;
+                    this.buttonText = 'Enable Push Messages';
+                  });
+              }).catch(function(e) {
+                console.error('Error thrown while unsubscribing from push messaging.', e);
+              });
+          });
+        */
+    }
 }]);
